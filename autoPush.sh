8cc22e2c21cd56372eba8516af49d0eb1171b0af
jslice <armslice@Arms-MacBook-Pro.local>
2020-02-19 15:17:44 -0800

    Auto Push - 02/19/20  15:17:44

--- autoPush.sh
+++ autoPush.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #arg 1 is commit comment
-ARG=""
-if [ "$@" -eq "" ]
-  then
-    $ARG="Committed "
-  else
-    $ARG=$1
-fi
+#ARG=""
+#if [ "$@" -eq "" ]
+#  then
+#    $ARG="Committed "
+#  else
+#    $ARG=$1
+#fi
 git add .
-git commit -m "$ARG"" - "$(date +"%D")"  "$(date +"%T")
+git commit -m "$1 - "$(date +"%D")"  "$(date +"%T")
 git push -u origin master
